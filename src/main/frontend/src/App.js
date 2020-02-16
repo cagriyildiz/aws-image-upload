6d34d0a1c0ca39f74075aaeb9d22bec1d659c7c1
cagriyildiz <cagriyild@gmail.com>
2020-02-16 12:55:43 +0300

    Fetch users

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import axios from 'axios';
+
 function App() {
+
+  const UserProfile = () => {
+
+    const [userProfiles, setUserProfiles] = useState([]);
+
+    const fetchUserProfiles = () => {
+      axios.get("http://localhost:8080/api/v1/user-profile")
+        .then(res => {
+          setUserProfiles(res.data);
+        });
+    };
+
+    useEffect(fetchUserProfiles, []);
+
+    return userProfiles.map((userProfile, index) => {
+      return (
+        <div key={index}>
+          <h1>{userProfile.username}</h1>
+          <p>{userProfile.userProfileId}</p>
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="App">
-
+      <UserProfile/>
     </div>
   );
 }
