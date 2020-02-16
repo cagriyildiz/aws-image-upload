a47408039d3954bd79de85c15ee3fe7d0186ff8f
cagriyildiz <cagriyild@gmail.com>
2020-02-16 20:48:07 +0300

    Fix the UUID for dummy users

--- src/main/java/com/cagriyildiz/awsimageupload/datastore/FakeUserProfileDataStore.java
+++ src/main/java/com/cagriyildiz/awsimageupload/datastore/FakeUserProfileDataStore.java
@@ -13,8 +13,14 @@ public class FakeUserProfileDataStore {
     private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
 
     static {
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "John Frusciante", null));
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "Cory Wong", null));
+        USER_PROFILES.add(
+                new UserProfile(UUID.fromString("41222097-04f0-4c6c-a031-9d5e7fe9da30"),
+                        "John Frusciante",
+                        null));
+        USER_PROFILES.add(
+                new UserProfile(UUID.fromString("951e8b1d-f617-4628-8a63-147bcee646c0"),
+                        "Cory Wong",
+                        null));
     }
 
     public List<UserProfile> getUserProfiles() {
